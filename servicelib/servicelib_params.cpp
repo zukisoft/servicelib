948099fecdba3a39014c65d2e2ef0d297551cc6c
zukisoft <djp952@gmail.com>
2014-06-05 09:08:42 -0400

    WIP 06.04.2014

--- servicelib/servicelib_params.cpp
+++ servicelib/servicelib_params.cpp
@@ -7,45 +7,80 @@
 namespace svctl {
 
 	// bool
+
+	// char
+
+	// wchar_t
+
+	// unsigned long --> 4
+
+	// unsigned long long --> 8
+
+	// float --> reinterpret unsigned long
+
+	// double --> reinterpret unsigned long long
+
+	// bool --> unsigned long
 	template<> bool parameter::ReadValue<bool, ServiceParameterFormat::Auto>(void)
 	{
-		size_t length;
-		ServiceParameterFormat type;
-		void* data = ReadValue(&length, &type);
-		if(!data) return false;
-
-		switch(type) {
-			case ServiceParameterFormat::Binary:
-			case ServiceParameterFormat::DoubleWord:
-			case ServiceParameterFormat::ExpandString:
-			case ServiceParameterFormat::QuadWord:
-			case ServiceParameterFormat::String:
-			case ServiceParameterFormat::StringArray:
-			default:
-				return false;
-		}
+		unsigned long value = ReadValue<unsigned long, ServiceParameterFormat::Auto>();
+		return (value != 0);
 	}
 
-	//template<> bool parameter::ReadValue<bool, ServiceParameterFormat::DoubleWord>(void)
-	//{
-	//	return ReadValue<bool, ServiceParameterFormat::Auto>();
-	//	//return ReadValue<unsigned int, ServiceParameterFormat::DoubleWord>() != 0;
-	//	//return false;
-	//}
+	// short --> unsigned long
+	template<> short parameter::ReadValue<short, ServiceParameterFormat::Auto>(void) 
+		{ return static_cast<short>(ReadValue<unsigned long, ServiceParameterFormat::Auto>()); }
 
-	template<> int parameter::ReadValue<int, ServiceParameterFormat::Auto>(void)
-	{
-		return -1;
-	}
+	// unsigned short --> unsigned long
+	template<> unsigned short parameter::ReadValue<unsigned short, ServiceParameterFormat::Auto>(void) 
+		{ return static_cast<unsigned short>(ReadValue<unsigned long, ServiceParameterFormat::Auto>()); }
+
+	// int --> unsigned long
+	template<> int parameter::ReadValue<int, ServiceParameterFormat::Auto>(void) 
+		{ return static_cast<int>(ReadValue<unsigned long, ServiceParameterFormat::Auto>()); }
+
+	// unsigned int --> unsigned long
+	template<> unsigned int parameter::ReadValue<unsigned int, ServiceParameterFormat::Auto>(void) 
+		{ return static_cast<unsigned int>(ReadValue<unsigned long, ServiceParameterFormat::Auto>()); }
+		
+	// long --> unsigned long
+	template<> long parameter::ReadValue<long, ServiceParameterFormat::Auto>(void) 
+		{ return static_cast<long>(ReadValue<unsigned long, ServiceParameterFormat::Auto>()); }
 		
 	template<> unsigned long parameter::ReadValue<unsigned long, ServiceParameterFormat::Auto>(void)
 	{
-		return 1;
-	}
-		
-	template<> unsigned int parameter::ReadValue<unsigned int, ServiceParameterFormat::Auto>(void)
-	{
-		return 1;
+		size_t					rawlength;			// Length of the raw registry value
+		ServiceParameterFormat	rawtype;			// Type/format of the raw registry value
+		unsigned long			value = 0;			// Resultant parameter value
+
+		// Read the raw registry value, length and type information from the registry
+		void* raw = ReadValue(&rawlength, &rawtype);
+		if(!raw) return value;
+
+		switch(rawtype) {
+
+			// Binary / DoubleWord / QuadWord --> Cast
+			case ServiceParameterFormat::Binary:
+			case ServiceParameterFormat::DoubleWord:
+			case ServiceParameterFormat::QuadWord:
+				if(rawlength >= sizeof(unsigned int)) value = *reinterpret_cast<unsigned long*>(raw);
+				break;
+
+			// String / ExpandString --> Convert
+			case ServiceParameterFormat::String:
+			case ServiceParameterFormat::ExpandString:
+				// TODO
+				break;
+
+			// StringArray --> Not Supported
+			case ServiceParameterFormat::StringArray:
+
+				// TODO
+				break;
+		}
+
+		FreeValue(raw);						// Release heap allocated buffer
+		return value;						// Return converted value
 	}
 };
 
